0885280b03b321dc4575339575893a4839d782ed
Sandeep26702 <ssharma931@rku.ac.in>
2025-03-29 22:15:58 +0530

    add redux toolkit

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import {Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -15,7 +21,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,41 +32,38 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         
          <input
            type='email'
            placeholder='email'
-           className='border p-3 rounded-lg'
+           className='p-3 border rounded-lg'
            id='email'
            onChange={handleChange}
          />
          <input
            type='password'
            placeholder='password'
-           className='border p-3 rounded-lg'
+           className='p-3 border rounded-lg'
            id='password'
            onChange={handleChange}
          />
  
          <button
            disabled={loading}
-           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+           className='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80'
          >
            {loading ? 'Loading...' : 'Sign In'}
          </button>
@@ -71,7 +74,7 @@ export default function SignIn() {
            <span className='text-blue-700'>Sign up</span>
          </Link>
        </div>
-       {error && <p className='text-red-500 mt-5'>{error}</p>}
+       {error && <p className='mt-5 text-red-500'>{error}</p>}
      </div>
       );
     }
